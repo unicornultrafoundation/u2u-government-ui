d2ca6a8e848a3115f31e023df312ea7c06a047aa
phidangnguyen <phidang0210@gmail.com>
2023-09-13 23:01:18 +0700

    update download link

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -10,12 +10,21 @@ export const Footer = () => {
           <img src={U2U} alt="" />
         </div>
         <div className="text-left text-sm font-semibold mb-2">Download U2U Wallet</div>
-        <div className="flex item-center justify-between gap-2 mb-6">
-          <img src={GgPlay} alt="u2u" className="w-6/12" />
-          <img src={AppStore} alt="u2u" className="w-6/12" />
+        <div className="flex item-center gap-4 mb-6">
+          <a href="https://play.google.com/store/apps/details?id=org.u2u.wallet" target="_blank" rel="noopener noreferrer">
+            <img src={GgPlay} alt="u2u" />
+          </a>
+          <a href="https://apps.apple.com/vn/app/u2u-wallet/id6446194312" target="_blank" rel="noopener noreferrer">
+            <img src={AppStore} alt="u2u" />
+          </a>
         </div>
         <div className="text-left text-sm font-semibold mb-2">Find us on:</div>
-        <div className="flex item-center justify-between">
+        <div className="flex item-center gap-4">
+          <a href="https://uniultra.xyz" target="_blank" rel="noopener noreferrer">
+            <div className="w-[35px] h-[35px] rounded-lg bg-pale leading-[35px] text-green text-lg">
+              <i className="fa fa fa-globe"></i>
+            </div>
+          </a>
           <a href="https://www.facebook.com/profile.php?id=100095507674287" target="_blank" rel="noopener noreferrer">
             <div className="w-[35px] h-[35px] rounded-lg bg-pale leading-[35px] text-green text-lg">
               <i className="fa fa-facebook"></i>
